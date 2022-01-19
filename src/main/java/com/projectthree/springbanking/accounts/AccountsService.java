b79cd117b4641c9a01c28c6948b53245e70ebe6f
Andy Yuen <yuena2036@gmail.com>
2022-01-19 09:49:27 -0800

    added jparepository

--- src/main/java/com/projectthree/springbanking/accounts/AccountsService.java
+++ src/main/java/com/projectthree/springbanking/accounts/AccountsService.java
@@ -1,9 +1,12 @@
 package com.projectthree.springbanking.accounts;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountsService {
 
+    @Autowired
+    private AccountsRepository accountsRepository;
 }
