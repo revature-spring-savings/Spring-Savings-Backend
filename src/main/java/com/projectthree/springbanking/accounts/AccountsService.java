8b3cc3ffccc80fd9965e5324256af3ccd0013d5c
HannahHowell <hrhowell59@hotmail.com>
2022-01-19 14:53:19 -0600

    working

--- src/main/java/com/projectthree/springbanking/accounts/AccountsService.java
+++ src/main/java/com/projectthree/springbanking/accounts/AccountsService.java
@@ -3,7 +3,6 @@ package com.projectthree.springbanking.accounts;
 
 import java.util.List;
 import java.util.Optional;
-import com.projectthree.springbanking.transactions.TransactionsEntity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,48 +27,11 @@ public class AccountsService {
 		
 	}
 	
-	public Optional <AccountsEntity> accountByID(Integer accountID) {
-		return accountsRepository.findById(accountID);
+//	public Optional <AccountsEntity> accountByID(Integer accountID) {
+//		return accountsRepository.findById(accountID);
 		
-	}
+//	}
 	
 	
 
-    @Autowired
-    private AccountsRepository accountsRepository;
-
-    // deposit money into account
-    // should take into account what type of account it is
-    public AccountsEntity deposit(TransactionsEntity transactionEntity, Integer accountID) {
-        // deposit amount
-        double depositAmt =  transactionEntity.getAmount();
-        // retrieve account from database
-        AccountsEntity accountEntity = accountsRepository.findById(accountID).get();
-        // current account balance;
-        double currBal = accountEntity.getAccountBalance();
-        // new account balance;
-        double newAcctBal = currBal + depositAmt;
-        // set new balance;
-        accountEntity.setAccountBalance(newAcctBal);
-        //save new balance into db
-        accountsRepository.save(accountEntity);
-        return accountEntity;
-    }
-
-    // withdraw money from account
-
-    public AccountsEntity withdraw(TransactionsEntity transactionEntity, Integer accountID) {
-        // withdraw amt
-        double withdrawAmt = transactionEntity.getAmount();
-        // retrieve account from db
-        AccountsEntity accountEntity = accountsRepository.findById(accountID).get();
-        // current account balance;
-        double currBal = accountEntity.getAccountBalance();
-        // new account balance;
-        double newAcctBal = currBal - withdrawAmt;
-        // set new balance;
-        accountEntity.setAccountBalance(newAcctBal);
-        accountsRepository.save(accountEntity);
-        return accountEntity;
-    }
 }
