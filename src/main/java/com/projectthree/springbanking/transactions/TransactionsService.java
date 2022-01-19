2016e0285875e6062d27f5de4cf6f814039eb706
stitchineer <stitchineer@yahoo.com>
2022-01-19 13:47:11 -0500

    writing conrtoller methods

--- src/main/java/com/projectthree/springbanking/transactions/TransactionsService.java
+++ src/main/java/com/projectthree/springbanking/transactions/TransactionsService.java
@@ -1,8 +1,35 @@
 package com.projectthree.springbanking.transactions;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class TransactionsService {
+
+	
+	@Autowired
+	private TransactionsRepository tr;
+	public TransactionsEntity transaction;
+	
+	
+	public List<TransactionsEntity> getAllTransactions() {
+		return tr.findAll();	
+	}
+
+	//Jeremy
+	public List<TransactionsEntity> getAllWithdrawalTransactions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	//Jeremy
+	public List<TransactionsEntity> getAllDepositTransactions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 
 }
