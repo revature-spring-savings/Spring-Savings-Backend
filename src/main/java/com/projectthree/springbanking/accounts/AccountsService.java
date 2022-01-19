cdc5f55fd637848d96be0997135e0b8437bffc16
HannahHowell <hrhowell59@hotmail.com>
2022-01-19 14:57:26 -0600

    working

--- src/main/java/com/projectthree/springbanking/accounts/AccountsService.java
+++ src/main/java/com/projectthree/springbanking/accounts/AccountsService.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projectthree.springbanking.transactions.TransactionsEntity;
+
 @Service
 public class AccountsService {
 	
@@ -31,6 +33,39 @@ public class AccountsService {
 //		return accountsRepository.findById(accountID);
 		
 //	}
+	
+	 public AccountsEntity deposit(TransactionsEntity transactionEntity, Integer accountID) {
+	        // deposit amount
+	        double depositAmt =  transactionEntity.getAmount();
+	        // retrieve account from database
+	        AccountsEntity accountEntity = accountsRepository.findById(accountID).get();
+	        // current account balance;
+	        double currBal = accountEntity.getAccountBalance();
+	        // new account balance;
+	        double newAcctBal = currBal + depositAmt;
+	        // set new balance;
+	        accountEntity.setAccountBalance(newAcctBal);
+	        //save new balance into db
+	        accountsRepository.save(accountEntity);
+	        return accountEntity;
+	    }
+
+	    // withdraw money from account
+
+	    public AccountsEntity withdraw(TransactionsEntity transactionEntity, Integer accountID) {
+	        // withdraw amt
+	        double withdrawAmt = transactionEntity.getAmount();
+	        // retrieve account from db
+	        AccountsEntity accountEntity = accountsRepository.findById(accountID).get();
+	        // current account balance;
+	        double currBal = accountEntity.getAccountBalance();
+	        // new account balance;
+	        double newAcctBal = currBal - withdrawAmt;
+	        // set new balance;
+	        accountEntity.setAccountBalance(newAcctBal);
+	        accountsRepository.save(accountEntity);
+	        return accountEntity;
+	    }
 	
 	
 
