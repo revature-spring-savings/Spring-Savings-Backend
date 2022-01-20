234db164690a4e5e45af81452c45e40233171b71
Daniel Lane <charles.lane@revature.net>
2022-01-20 13:33:02 -0600

    just pulled

--- src/main/java/com/projectthree/springbanking/users/UsersEntity.java
+++ src/main/java/com/projectthree/springbanking/users/UsersEntity.java
@@ -8,13 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Entity
 @Data
-@Getter
-@Setter
 @Table  (name = "users")
 public class UsersEntity {
 	
