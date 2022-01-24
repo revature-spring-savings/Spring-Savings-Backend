749c7d16a4b54b8f0d2730ee291519f9a55d5f65
stitchineer <stitchineer@yahoo.com>
2022-01-24 15:28:05 -0500

    transactions

--- src/main/java/com/projectthree/springbanking/transactions/TransactionsEntity.java
+++ src/main/java/com/projectthree/springbanking/transactions/TransactionsEntity.java
@@ -36,13 +36,15 @@ public class TransactionsEntity {
 	@Column(name="transaction_note")
 	private String transactionNote; // paycheck from Revature OR electricity bill OR transfer from acct 1 to acct 2
 
-	@ManyToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	//@ManyToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name = "user_id", referencedColumnName="user_id", nullable=false, insertable=false, updatable=false)
 	@JsonIgnore
 	@ToString.Exclude
 	private UsersEntity usersEntity;
 
-	@ManyToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	//@ManyToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name = "account_id", referencedColumnName="account_id", nullable=false, insertable=false, updatable=false)
 	@JsonIgnore
 	@ToString.Exclude
