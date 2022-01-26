a876b21517892c3e8829a83aadf0fe3480bebd4a
stitchineer <stitchineer@yahoo.com>
2022-01-26 18:29:25 -0500

    mockito tests

---
+++ src/test/java/com/projectthree/springbanking/transcations/TransactionsServiceTest.java
@@ -0,0 +1,43 @@
+package com.projectthree.springbanking.transcations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.projectthree.springbanking.SpringBankingApplication;
+import com.projectthree.springbanking.transactions.TransactionsEntity;
+import com.projectthree.springbanking.transactions.TransactionsService;
+
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RunWith(MockitoJUnitRunner.class)
+class TransactionsServiceTest {
+	
+	@Mock
+	TransactionsService ts = org.mockito.Mockito.mock(TransactionsService.class);
+	
+	@Test
+	public void getByAccountIDMockTest() {
+		when(ts.getAllTransactionsByAccountID(5)).thenReturn(Stream
+				.of(new TransactionsEntity(), new TransactionsEntity()).collect(Collectors.toList()));
+		assertEquals(2,  ts.getAllTransactionsByAccountID(5).size());		
+	}
+	
+	@Test
+	public void getByAccountIDTest() {
+		int i = ts.getAllTransactionsByAccountID(1).size();
+		List<TransactionsEntity> listT = ts.getAllTransactionsByAccountID(1);
+		assertEquals(i,  listT.size());		
+	}
+	
+	
+
+}
